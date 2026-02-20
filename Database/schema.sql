b2f7c877da5ac1e5b7e8e99994cfa68e23f3b5b7
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-02-20 07:50:52 +0530

    feat: start work on some web projects and starting the DATABASE journey

---
+++ Database/schema.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS school_system;
+USE school_system;
+CREATE TABLE student (
+    student_id Int PRIMARY key auto_increment,
+    student_name VARCHAR(50),
+    age INT
+);
+USE school_system;
+INSERT INTO student (student_name, age)
+values ('Rahul', 20),('Shubham',19);
+select * from student;
+use school_system;
+update student
+set student_name = 'Sisimanu'
+where student_name like 'Rahul%';
+SELECT * from student;
