8271033cfa432cf4ecc736b42b4b54c1dae88dce
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-03-07 06:52:42 +0530

    feat: implement constraints to create a table

--- Database/Day02.sql
+++ Database/Day02.sql
@@ -7,3 +7,16 @@ insert into self1 values(2204, 'COA', "Sisimanu", '10-11-2017');
 insert into self1 values(2214, 'MATHEMATICS', "RD. Sharma", '15-09-2000');
 select * from self1;
 select * from self1 where book_id <= 2230;
+
+# Constraints 
+create table staffs(
+    id int not null unique,
+    name varchar(100) not null, 
+    email varchar(150) not null unique,
+    age tinyint check (age>=18),
+    status boolean default 1
+);
+desc staffs;
+insert into staffs(id,name,email,age) 
+values(123, 'Dipankar', "Dipankar123@icloud.com", 19);
+select * from staffs;
