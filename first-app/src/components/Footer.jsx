25cab771484c7c7ca5607def89172d124e6fea30
Shubham Jana <devshubhamjana@gmail.com>
2026-08-21 19:24:10 +0000

    feat: understand the tag creating and linking css and jsx file togater

---
+++ first-app/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () =>{
+    return (
+        <div>
+            @copy copyright CoderTwine | All rights are reserved
+        </div>
+    )
+}
+export default Footer
